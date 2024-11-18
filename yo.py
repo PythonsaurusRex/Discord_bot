316ed8bd5effd4f62f34deda1cabf7a3177a22e1
PythonsaurusRex <alejodiaztopia@gmail.com>
2024-11-18 08:23:47 -0600

    Create yo.py

---
+++ yo.py
@@ -0,0 +1,5 @@
+import random
+def alejo():
+    yo = ["Estoy en el noveno grado","Me gusta el sushi","Estoy en una escuela en linea","Me gustan los perros","Me gustan las matematicas"]
+    sobre_mi = random.choice(yo)
+    return sobre_mi
